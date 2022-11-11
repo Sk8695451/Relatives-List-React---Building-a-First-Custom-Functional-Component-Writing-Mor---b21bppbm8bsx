9b780a9af34db4d16d7a94f63f57bf52563e676e
unknown <sk8695451>
2022-11-11 22:43:44 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,13 @@ class App extends Component {
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+                
+                <ol>
+                    <li key="relativeListItem1">relativeListItem1</li>
+                    <li key="relativeListItem2">relativeListItem2</li>
+                    <li key="relativeListItem3">relativeListItem3</li>
+                </ol>
+                
             </div>
         )
     }
